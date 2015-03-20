61ae384bb08c9112c8203b2f34978dc0835ba38f
VooDooBlooD <mdan.hagh@gmail.com>
2015-03-20 18:45:23 +0330

    Added threads

--- graph_drawer.py
+++ graph_drawer.py
@@ -6,6 +6,6 @@ def draw(edges, labels, colors):
 	graph = Graph(edges=edges, directed=True)
 
 	graph.vs["label"] = labels
-	#graph.vs["color"] = colors
+	graph.vs["color"] = colors
 
 	plot(graph, bbox=(1000, 1000), margin=100)
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 #! /urs/bin/pyhton
 
 import urllib2
+import threading
 
 import funcs
 import graph_drawer
@@ -24,6 +25,19 @@ def print_graph(graph, ver2usr):
 		print ver2usr[i], '\t', graph[i]
 	print '-' * 100
 
+def analyse_users(ver2usr, ver2type, index, threads_num, flags):
+	opener = setup_opener()
+
+	i = index
+	while flags[index]:
+		if i >= len(ver2usr):
+			continue
+
+		tweets = funcs.get_tweets(opener, ver2usr[i])
+		ver2type[i] = funcs.analyse_tweets(tweets)
+		i += threads_num
+
+
 def create_graph(opener, username, max_depth):
 	##graph = [[]]
 	edges = []
@@ -31,7 +45,15 @@ def create_graph(opener, username, max_depth):
 	usr_info = funcs.get_name_info(opener, username)
 	usr2ver = {usr_info: 0}
 	ver2usr = [str(usr_info[0])]
+	ver2type = [None]
 	bfs = [(usr_info, 0)]
+
+	threads = []
+	threads_num = 4
+	flags = [True for i in range(threads_num)]
+	for i in range(threads_num):
+		threads.append(threading.Thread(None, analyse_users, (), (ver2usr, ver2type, i, threads_num, flags)))
+		threads[-1].start()
 
 	while len(bfs):
 		user = bfs[0][0]
@@ -43,22 +65,26 @@ def create_graph(opener, username, max_depth):
 
 
 		followings = funcs.get_followings(opener, user[0])
-		followers  = funcs.get_followers(opener, user[0])
 
 		for f in followings:
 			if not f in usr2ver:
 				usr2ver[f] = len(usr2ver)
 				ver2usr.append(str(f[0]))
+				ver2type.append(None)
 				##graph.append([])
 
 			bfs.append((f, depth + 1))
 			##graph[usr2ver[user]].append(usr2ver[f])
 			edges.append((usr2ver[user], usr2ver[f]))
 
+
+		followers  = funcs.get_followers(opener, user[0])
+
 		for f in followers:
 			if not f in usr2ver:
 				usr2ver[f] = len(usr2ver)
 				ver2usr.append(str(f[0]))
+				ver2type.append(None)
 				##graph.append([])
 
 			bfs.append((f, depth + 1))
@@ -69,13 +95,22 @@ def create_graph(opener, username, max_depth):
 		#graph_drawer.draw(edges, ver2usr, [])
 
 
-	return edges, ver2usr
+	while None in ver2type:
+		continue
+
+	for i in range(threads_num):
+		flags[i] = False
+
+	return edges, ver2usr, ver2type
 
 
 def run_scraper(username, max_depth=2):
-	baseurl = "https://twitter.com/"
-
 	opener = setup_opener()
 
-	edges, ver2usr = create_graph(opener, username, max_depth)
-	graph_drawer.draw(edges, ver2usr, [])
+	edges, ver2usr, ver2type = create_graph(opener, username, max_depth)
+	for i in range(len(ver2type)):
+		if ver2type[i]:
+			ver2type[i] = 'blue'
+		else:
+			ver2type[i] = 'white'
+	graph_drawer.draw(edges, ver2usr, ver2type)
